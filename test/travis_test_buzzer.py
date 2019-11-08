9cedcf027e183af9477585e045d45e69b4218b77
Keisuke Ichida <ichida@ube-k.ac.jp>
2019-11-08 16:59:29 +0900

    Add a file

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -12,11 +12,11 @@ if __name__== '__main__':
    time.sleep(3)
    rospy.init_node('travis_test_buzzer')
    rostest.rosrun('pimouse_ros','travis_test_buzzer',BuzzerTest)
-        def test_node_exist(self):
-            nodes = rosnode.get_node_names()
-            self.assertIn('/buzzer',nodes, "node does not exist")
+       def test_node_exist(self):
+           nodes = rosnode.get_node_names()
+           self.assertIn('/buzzer',nodes, "node does not exist")
 
 if __name__ == '__main__':
-        time.sleep(3)
-        rospy.init_node('travis_test_buzzer')
-.rosrun('pimout_node_exist(self):e_ros','travis_test_buzzer',BuzzerTest)
+   time.sleep(3)
+   rospy.init_node('travis_test_buzzer')
+   .rosrun('pimout_node_exist(self):e_ros','travis_test_buzzer',BuzzerTest)
