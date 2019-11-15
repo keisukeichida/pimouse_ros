27c81bb3356a29e12a0149de0c22b87b1923129f
Keisuke Ichida <ichida@ube-k.ac.jp>
2019-11-15 14:07:22 +0900

    Add a modified file

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -35,8 +35,10 @@ class BuzzerTest(unittest.TestCase):
         self.client.wait_for_result()
 
         self.assertTrue(self.client.get_result(),"invalid result")
-        self.assertEqual(goal.freqs,self.device_values,"invalid feedback:" + ",".join([str(e) for e in self.device_values]))
+        self.assertEqual(goal.freqs,self.device_values,"invalid feedback:"
+                + ",".join([str(e) for e in self.device_values]))
 
+        ###preemption###
         self.device_values = []
         self.client.send_goal(goal,feedback_cb=self.feedback_cb)
         self.client.wait_for_result(rospy.Duration.from_sec(0.5))
